80d7d1fb74b7c32a33825abd5234c86e2e52e5d1
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-03-26 20:34:34 -0500

    Develop (#5)
    * Feature/brayan narvaez integrante1 (#2)
    
    * feat: create class Billetera.java
    
    * feat: create attributes and constructor in class Billetera.java
    
    * feat: create method mostrarInfo of the class Billetera.java
    
    * feat: create method depositar of the class Billetera.jva
    
    * feat: create method retirar of the class Billetera.java
    
    * Feature/edwincusin integrante2 (#3)
    
    * feat: add file Transaccion
    
    * feat: add atributos of Transaccion
    
    * feat: add constructores in class Transaccion
    
    * feat: add metodos get and set in Class Transaccion
    
    * feat: add metodo mostrarDetalle()  in Class Transaccion
    
    * feat: add metodo aplicarComision(double
    porcentaje)  in Class Transaccion
    
    * feat: add metodo marcarFallida()   in Class Transaccion
    
    * Feature/angelmorales integrante3 (#4)
    
    * feet: add class Contacto
    
    * feet: add contructor Contacto
    
    * feet: add mostrarContacto
    
    * feet : add method marcarFavorito
    
    * feet: add correcttions
    
    * feet: add method enviarDinero
    
    ---------
    
    Co-authored-by: EDWINCSN <30878024+edwincusin@users.noreply.github.com>
    Co-authored-by: angel-hub31 <moralesmales1@hotmail.com>

---
+++ Transaccion.java
@@ -0,0 +1,82 @@
+public class Transaccion {
+    //ATRIBUTOS
+	private  String tipo;
+	private double monto;
+	private String descripcion;
+	private boolean exitosa;
+	
+    //CONSTRUCTOR VACIO
+	public Transaccion() {
+	}
+	
+	//CONSTRUCTOR CON PARAMETROS
+	public Transaccion(String tipo, double monto, String descripcion) {
+		super();
+		this.tipo = tipo;
+		this.monto = monto;
+		this.descripcion = descripcion;
+		this.exitosa=true;
+	}
+
+	//METODOS GET Y SET
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public boolean isExitosa() {
+		return exitosa;
+	}
+
+	public void setExitosa(boolean exitosa) {
+		this.exitosa = exitosa;
+	}
+    
+	//METODO MOSTRAR DETALLE 
+	public void mostrarDetalle() {
+		System.out.println("---- Detalle de Transacción ----");
+		System.out.println("Tipo:" +tipo);
+		System.out.println("Monto: "+monto);
+		System.out.println("Descripción: "+descripcion);
+		if(exitosa) {
+			System.out.println("Estado: Exitosa");
+		}else {
+			System.out.println("Estado: Fallida");
+		}		
+	}
+    
+	//METODO APLICAR COMISION SEGUN PORCENTAJE
+	public void aplicarComision(double porcentaje) {
+		double comision=(monto*porcentaje)/100;
+		double montoTotal=comision+monto;
+		System.out.println("Comision( "+porcentaje+" %) : "+comision);
+		System.out.println("Monto total con comision: "+montoTotal);		
+	}
+
+	//METODO MARCAR FALLIDA 
+	public void marcarFallida() {
+		exitosa=false;
+		System.out.println("Transacción marcada como fallida.");
+		System.out.println("Monto afectado: "+monto);
+	}
+
+}
